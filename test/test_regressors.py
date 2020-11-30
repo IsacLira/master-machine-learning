18a98e45b5564300584de8dc1fe0820236337672
Isac Lira <isac_lira@apple.com>
2020-11-30 00:08:33 -0300

    Build MLP regressor using Pytorch

--- test/test_regressors.py
+++ test/test_regressors.py
@@ -1,5 +1,6 @@
 from regressors.least_square import LeastSquareRegressor
 from regressors.adaline_regressor import ADALINERegressor
+from regressors.mlp import build_model, train_model, predict
 from sklearn.datasets import make_regression
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split as split
@@ -7,12 +8,35 @@ from sklearn.model_selection import train_test_split as split
 X, y = make_regression(1000, 20, noise=50)
 X_train, X_test, y_train, y_test = split(X, y, train_size=.7)
 
-ls_reg = LeastSquareRegressor()
-ls_reg.fit(X_train, y_train)
-score = r2_score(y_test, ls_reg.predict(X_test))
-print(f'Least Square performance: {score}')
+# ls_reg = LeastSquareRegressor()
+# ls_reg.fit(X_train, y_train)
+# score = r2_score(y_test, ls_reg.predict(X_test))
+# print(f'Least Square performance: {score}')
 
-ada_reg = ADALINERegressor()
-ada_reg.fit(X_train, y_train, alpha=0.001, epoches=50)
-score = r2_score(y_test, ada_reg.predict(X_test))
-print(f'ADALINE Regressor performance: {score}')
+# ada_reg = ADALINERegressor()
+# ada_reg.fit(X_train, y_train, alpha=0.001, epoches=50)
+# score = r2_score(y_test, ada_reg.predict(X_test))
+# print(f'ADALINE Regressor performance: {score}')
+
+mlp = build_model(n_feats=X_train.shape[1],
+                  layers_size=[200])
+train_model(X=X_train,
+            y=y_train,
+            model=mlp,
+            epoches=100,
+            batch_size=10)
+
+score = r2_score(y_test, predict(X_test, mlp))
+print(f'MLP (1 hidden layer) performance: {score}')
+
+
+mlp = build_model(n_feats=X_train.shape[1],
+                  layers_size=[100, 100])
+train_model(X=X_train,
+            y=y_train,
+            model=mlp,
+            epoches=200,
+            batch_size=10)
+
+score = r2_score(y_test, predict(X_test, mlp))
+print(f'MLP (2 hidden layers) performance: {score}')
